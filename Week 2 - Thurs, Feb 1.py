9a1cb4f4241eab75ca3b2fba1d688c09f260b23e
Nathan <nsatterfield@fwparker.org>
2018-02-01 14:22:49 -0600

    New notes

--- Week 2 - Thurs, Feb 1.py
+++ Week 2 - Thurs, Feb 1.py
@@ -5,16 +5,27 @@ by Nathan Satterfield
 '''
 from random import *       # random is a library or module, * is the wildcard
 from math import pi
-from math import *
+# from math import *
+import my_module
 # imports go to the top of your program
 
 # when you use keyword from, you are importing directly into your file
 # (no need to use random.randrange, just use randrange)
 
 # Functions and Modules
-print(randrange(100))
-print(random())
-print(pi)
-print(e)
-e = 5
-print(e)
+
+
+if __name__ == "__main__":
+    '''
+    This code only runs if this is the executed code/file.
+    '''
+    print(randrange(100))
+    print(random())
+    print(pi)
+    
+    e = 5
+    print(e)
+    print("This is period", my_module.period)
+    my_module.hello("Nathan")
+    product, sum = (my_module.product_sum(10, 20))
+    print(product, sum)
---
+++ my_module.py
@@ -0,0 +1,35 @@
+period = "B"
+
+
+# parameters are inputs to the function
+# they are assigned locally (scope)
+
+
+def hello(name):
+    '''
+    Says hello to the name
+    :param name: 
+    :return: 
+    '''
+
+    print("Hello there,", name + ".")
+
+
+def product_sum(n1, n2):
+    '''
+   Returns Product and sum of two numbers
+    :param n1:
+    :param n2:
+    :return:
+    '''
+
+    product = n1 * n2
+    sum = n1 + n2
+    return product, sum
+
+
+if __name__ == "__main__":
+    hello("B Period!")
+    print(product_sum(4, 5))
+
+
