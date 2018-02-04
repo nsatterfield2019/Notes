1e98ec28f78ca4ddf881e0a0328d01cef1a24e5a
Nathan <nsatterfield@fwparker.org>
2018-02-04 12:21:59 -0600

    Updating

--- Week 2 - Thurs, Feb 1.py
+++ Week 2 - Thurs, Feb 1.py
@@ -35,3 +35,12 @@ if __name__ == "__main__":
 
     my_module.hello(name = "Francis")
     my_module.hello()
+
+
+for a in range(1, 10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(1, 10):
+                num = str(a) + str(b) + str(c) + str (d)
+                num_reverse = str(d) + str(c) + str(b) + str(a)
+                print(num, num_reverse)
