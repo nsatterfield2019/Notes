de2d04e867c4ed28cbd88bf7dae7fa344e013436
Nathan <nsatterfield@fwparker.org>
2018-02-12 09:51:00 -0600

    Updating

---
+++ Week 3 - Tuesday, Feb 6.py
@@ -0,0 +1,16 @@
+# LISTS and LIST COMPREHENSIONS
+
+print("Francis Parker".lower())
+
+name = "Francis Parker"
+
+for char in name:
+    print(char.lower(), char)
+
+if 3 != 4: # not equal too
+    print("Yep")
+
+def sum_product(n1, n2):
+    sum = n1 + n2
+    product = n1 * n2
+    print(sum, product)
