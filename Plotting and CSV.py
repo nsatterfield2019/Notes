26ed382177ff78075f2056625ed6824a4b443bc5
Nathan <nsatterfield@fwparker.org>
2018-03-22 17:12:25 -0500

    Updated

--- Plotting and CSV.py
+++ Plotting and CSV.py
@@ -28,5 +28,50 @@ plt.ylabel("Computer Users")
 
 plt.xticks(library_number, names, rotation=90, fontsize=7) # plotted data, text to plot, rotation
 
+# Make a line graph of comp users at three libraries by month
+
+print(data)
+month_names = data[0][1:-1]
+print(month_names)
+
+month_numbers = [x for x in range(12)]
+print(month_numbers)
+
+library_names = [x[0] for x in data]
+print(library_names)
+
+bwp_data =  data[library_names.index('Bucktown-Wicker Park')]
+bwp_data.pop(0)
+bwp_data.pop()
+bwp_data = [int(x) for x in bwp_data]
+print(bwp_data)
+
+
+
+lp_data =  data[library_names.index('Lincoln Park')]
+lp_data.pop(0)
+lp_data.pop()
+lp_data = [int(x) for x in lp_data]
+print(lp_data)
+
+
+
+hp_data =  data[library_names.index('Humboldt Park')]
+hp_data.pop(0)
+hp_data.pop()
+hp_data = [int(x) for x in hp_data]
+print(hp_data)
+
+
+plt.figure(2, tight_layout=True)
+
+plt.plot(month_numbers, bwp_data, label="Bucktown-Wicker Park")
+plt.plot(month_numbers, hp_data, label='Humboldt Park')
+plt.plot(month_numbers, lp_data, label='Lincoln Park')
+plt.xticks(month_numbers, month_names, rotation=45)
+
+plt.legend(bbox_to_anchor=(0, 1), loc="upper left")
+
 plt.show()
+
 
